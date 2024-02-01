d8722069bebd99c8b87fb7b72847832f67006123
Nilo <cyprianopartner@gmail.com>
2024-02-01 15:36:15 -0300

    feat: basic configs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BrowserModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
