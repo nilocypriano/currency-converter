f801e277e097fc6bc2b2fefc338f72404a8d37fa
Nilo <cyprianopartner@gmail.com>
2024-02-06 15:52:59 -0300

    feat: create general error interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { ErrorInterceptor } from './shared/utils/interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptorsFromDi()
+    ),
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+  ]
 };
---
+++ src/app/shared/utils/interceptors/error.interceptor.ts
@@ -0,0 +1,15 @@
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, catchError, throwError } from "rxjs";
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        console.log('HTTP Error:', error)
+        return throwError(() => error.message)
+      })
+    )
+  }
+}
